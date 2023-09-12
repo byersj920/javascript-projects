0f7e855715f2573524c215f164f2191722819b7e
Jonathan <byersj920@gmail.com>
2023-09-12 12:50:15 -0500

    fijwejiof

---
+++ Testing Grounds.js
@@ -0,0 +1,13 @@
+console.log(Boolean("true"));
+console.log(Boolean('TRUE'));
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(''));
+console.log(Boolean('LaunchCode'));
+let num = 8;
+
+if (num % 2 === 0) {
+    if (num % 2 === 1) {
+        console.log("odd");
+    }
+}
