7860a5d0092017080c211c759e0cb4fb808e2375
Jonathan <byersj920@gmail.com>
2023-10-01 13:17:52 -0500

    Studio Submission

--- objects-and-math/chapter-examples/ForInLoop.js
+++ objects-and-math/chapter-examples/ForInLoop.js
@@ -7,3 +7,16 @@ let tortoiseOne = {
 };
 
 // Using a for..in loop, iterate through each property in the tortoiseOne object and print the value to the console.
+
+for (item in tortoiseOne){
+   console.log(`${item}: ${tortoiseOne[item]}`);
+}
+
+console.log(Math.round(-3.51));
+console.log(Math.trunc(-3.87));
+
+let num = Math.floor(Math.random()*10);
+
+console.log(num);
+console.log(Math.random());
+console.log(Math.round(0.1));
