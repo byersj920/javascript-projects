b00273ed0c27063e49f447a323eaa76ee0249f52
Jonathan Byers <byersj920@gmail.com>
2023-10-11 15:51:27 -0500

    Exercise Submission

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -5,3 +5,12 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+
+let autoDivider = function(numerator, denominator){
+    if (numerator === 0){
+        throw Error("You can't divide by zero!");
+    }
+    return (numerator/denominator);
+};
+
+console.log(autoDivider(0, 3));
