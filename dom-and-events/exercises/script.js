818631c9d9dd5eedb4169771fab35f09dad64d52
Jonathan <byersj920@gmail.com>
2023-11-01 14:02:51 -0500

    Exercise Submission

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,22 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+   button.addEventListener('click', function(){
+    paragraph.innerHTML = "Houston! We have liftoff!";
+   });
+
+   missionAbort.addEventListener('mouseover', function (){
+    missionAbort.style.backgroundColor = 'red';
+   });
+    missionAbort.addEventListener('mouseleave', function(){
+        missionAbort.style.backgroundColor = '';
+    });
+    missionAbort.addEventListener('click', function(){
+        response = window.confirm("Are you sure you want to abort the mission?? This might be kinda bad...");
+        if (response === true){
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home...'
+        }
+    });
 }
 
 window.addEventListener("load", init);
